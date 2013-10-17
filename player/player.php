40a6e3539ac897368224d075f851c6930f541153
Warlot Pierre-Quentin <pwarlot@enssat.fr>
2013-10-17 18:21:55 +0200

    debug player

--- player/player.php
+++ player/player.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
 <?php
 	$film = $_GET["f"];
 	
@@ -6,15 +9,22 @@
 	else
 		$q = "hd";
 ?>
-<style>
-body {
-	margin:0;
-	padding:0;
-	background:black;
-}
-</style>
-<link href="player.css" rel="stylesheet">
-<script src="player.js"></script>
+	<style>
+	body {
+		margin:0;
+		padding:0;
+		background:black;
+	}
+	</style>
+	<link href="video-js.min.css" rel="stylesheet">
+	<script src="video.js"></script>
+	<script>
+		videojs.options.flash.swf = "video-js.swf";
+	</script>
+</head>
+<body>
 <video id="my_video_1" class="video-js vjs-default-skin" controls preload="auto" width="940" height="530" data-setup="{}">
   <source src="../videos/<?php if($q=='sd') echo 'sd/'; echo $film; ?>.mp4" type='video/mp4'>
 </video>
+</body>
+</html>
